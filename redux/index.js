b11b0a9f256d234ec1a7ee5bc59c919ae6816216
AI-Ashik <arifulislamashik07@gmail.com>
2024-01-30 15:06:30 +0600

    created redux store

--- redux/index.js
+++ redux/index.js
@@ -1,40 +1,51 @@
+// Importing createStore function from the Redux library
+const { createStore } = require("redux");
+
+// Defining action types
 const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
-const ADD_USER = "ADD_USER";
 
+// Initial state for the counter
 const initialCounterState = {
-  counter: 0,
+  count: 0,
 };
 
-// action - Object(2) - type, payload
-const incrementCounter = () => {
-  return {
-    type: INCREMENT,
-  };
-};
+// Action creator for incrementing the counter
+const incrementCounter = () => ({ type: INCREMENT });
 
+// Action creator for decrementing the counter
+const decrementCounter = () => ({ type: DECREMENT });
 
-
-const decrementCounter = () => {
-  return {
-    type: DECREMENT,
-  };
-};
-
+// Reducer function to manage state changes
 const counterReducer = (state = initialCounterState, action) => {
   switch (action.type) {
     case INCREMENT:
+      // Increment the count in the state
       return {
         ...state,
-        count: state.counter + 1,
+        count: state.count + 1,
       };
     case DECREMENT:
+      // Decrement the count in the state
       return {
         ...state,
-        count: state.counter - 1,
+        count: state.count - 1,
       };
-
     default:
+      // Return the current state if no action type matches
       return state;
   }
 };
+
+// Creating a Redux store with the counterReducer
+const store = createStore(counterReducer);
+
+// Subscribe to store changes and log the current state
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// Dispatching actions to increment and decrement the counter
+store.dispatch(incrementCounter());
+store.dispatch(incrementCounter());
+store.dispatch(decrementCounter());
