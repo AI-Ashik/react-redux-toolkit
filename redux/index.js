c0049f542b6384764866c3e6fe391df8caf8ee3f
AI-Ashik <arifulislamashik07@gmail.com>
2024-01-30 15:25:21 +0600

    added reset counter

--- redux/index.js
+++ redux/index.js
@@ -1,9 +1,8 @@
-// Importing createStore function from the Redux library
 const { createStore } = require("redux");
 
-// Defining action types
 const INCREMENT = "INCREMENT";
 const DECREMENT = "DECREMENT";
+const RESET = "RESET";
 
 // Initial state for the counter
 const initialCounterState = {
@@ -16,36 +15,40 @@ const incrementCounter = () => ({ type: INCREMENT });
 // Action creator for decrementing the counter
 const decrementCounter = () => ({ type: DECREMENT });
 
-// Reducer function to manage state changes
+// Action creator for reset the counter
+const resetCounter = () => ({ type: RESET });
+
 const counterReducer = (state = initialCounterState, action) => {
   switch (action.type) {
     case INCREMENT:
-      // Increment the count in the state
       return {
         ...state,
         count: state.count + 1,
       };
     case DECREMENT:
-      // Decrement the count in the state
       return {
         ...state,
         count: state.count - 1,
       };
+    case RESET:
+      // Decrement the count in the state
+      return {
+        ...state,
+        count: 0,
+      };
+
     default:
-      // Return the current state if no action type matches
       return state;
   }
 };
 
-// Creating a Redux store with the counterReducer
 const store = createStore(counterReducer);
 
-// Subscribe to store changes and log the current state
 store.subscribe(() => {
   console.log(store.getState());
 });
 
-// Dispatching actions to increment and decrement the counter
 store.dispatch(incrementCounter());
 store.dispatch(incrementCounter());
 store.dispatch(decrementCounter());
+store.dispatch(resetCounter());
