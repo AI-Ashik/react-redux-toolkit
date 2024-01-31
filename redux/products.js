39278faf352fe3694a1c9a423ad82b6d456b9881
AI-Ashik <arifulislamashik07@gmail.com>
2024-01-31 21:30:34 +0600

    Revert "added cart reducer and combined two reducers"
    This reverts commit 4290b915cc38d31fba91e79692cb898330e7d423.

--- redux/products.js
+++ redux/products.js
@@ -1,26 +1,15 @@
-const { createStore, combineReducers } = require("redux");
+const { createStore } = require("redux");
 
-// defining products constants
+// defining constants
 const GET_PRODUCTS = "GET_PRODUCTS";
 const ADD_PRODUCTS = "ADD_PRODUCTS";
 
-// defining products constants
-const GET_CART = "GET_CART";
-const ADD_CART = "ADD_CART";
-
-// initial product States
+// initialStates
 const productInitialState = {
   products: ["Mango", "Apple", "iPhone"],
   numOfProducts: 3,
 };
 
-// initial cart States
-const cartInitialState = {
-  cart: ["TV"],
-  numOfProducts: 1,
-};
-
-// products actions
 const getProducts = () => {
   return {
     type: GET_PRODUCTS,
@@ -34,21 +23,7 @@ const addProducts = (product) => {
   };
 };
 
-// cart actions
-const getCart = () => {
-  return {
-    type: GET_CART,
-  };
-};
-
-const addCart = (product) => {
-  return {
-    type: ADD_CART,
-    payload: product,
-  };
-};
-
-// product reducer
+// reducer
 const productsReducer = (state = productInitialState, action) => {
   switch (action.type) {
     case GET_PRODUCTS:
@@ -66,42 +41,12 @@ const productsReducer = (state = productInitialState, action) => {
   }
 };
 
-// cart reducer
-const cartReducer = (state = cartInitialState, action) => {
-  switch (action.type) {
-    case GET_CART:
-      return {
-        ...state,
-      };
-    case ADD_CART:
-      return {
-        cart: [...state.cart, action.payload],
-        numOfProducts: state.numOfProducts + 1,
-      };
+const store = createStore(productsReducer);
 
-    default:
-      return state;
-  }
-};
-
-// combining two reducers
-const rootReducer = combineReducers({
-  productsReducer,
-  cartReducer,
-});
-
-// created redux store
-const store = createStore(rootReducer);
-
-// subscribe redux store
 store.subscribe(() => {
   console.log(store.getState());
 });
 
-// calling the product actions
 store.dispatch(getProducts());
 store.dispatch(addProducts("Avacado"));
-
-// calling the cart actions
-store.dispatch(getCart());
-store.dispatch(addCart("Monitor"));
+store.dispatch(addProducts("Avacado"));
