a1b4ed8a0a3465a6f664ba2642f09428dec820a5
AI-Ashik <arifulislamashik07@gmail.com>
2024-02-04 12:53:37 +0600

    created projext setup for resux-toolkit ofr data fetching

---
+++ fetch api using redux-toolkit/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import CounterView from "./features/counter/CounterView";
+
+function App() {
+  return (
+    <>
+      <h2 id="root">Counter App</h2>
+      <CounterView />
+    </>
+  );
+}
+
+export default App;
