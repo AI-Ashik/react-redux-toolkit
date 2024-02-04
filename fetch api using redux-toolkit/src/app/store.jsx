2aaa31dda3a17c31ab97b1eda9ffebc46355b388
AI-Ashik <arifulislamashik07@gmail.com>
2024-02-04 13:16:06 +0600

    added the postReducer to the store

--- fetch api using redux-toolkit/src/app/store.jsx
+++ fetch api using redux-toolkit/src/app/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
+import postReducer from "../posts/postSlice";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    post: postReducer,
   },
 });
 
