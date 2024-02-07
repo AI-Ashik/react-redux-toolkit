acd8cef48b343fd89219d5a96507e3830d9a9294
AI-Ashik <arifulislamashik07@gmail.com>
2024-02-07 20:54:27 +0600

    coded the bookview page and added styling

--- react-redux-toolkit-crud-app/src/features/books/BooksView.jsx
+++ react-redux-toolkit-crud-app/src/features/books/BooksView.jsx
@@ -1,5 +1,44 @@
+import { useSelector } from "react-redux";
+import styles from "./booksView.module.css"; // Import module-level CSS
+
 const BooksView = () => {
-  return <div>BooksView</div>;
+  const books = useSelector((state) => state.bookReducer.books);
+
+  return (
+    <div>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {books &&
+            books.map((book) => {
+              const { id, title, author } = book;
+              return (
+                <tr key={id}>
+                  <td>{id}</td>
+                  <td>{title}</td>
+                  <td>{author}</td>
+                  <td>
+                    <button className={`${styles.button} ${styles.edit}`}>
+                      Edit
+                    </button>
+                    <button className={`${styles.button} ${styles.delete}`}>
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default BooksView;
