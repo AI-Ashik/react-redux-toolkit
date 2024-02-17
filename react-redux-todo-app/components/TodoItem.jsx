1fc3146cc9ac94bb9ed6f9257be8e7ae4fae0986
AI-Ashik <arifulislamashik07@gmail.com>
2024-02-17 22:10:41 +0600

    created toggleComplete action

--- react-redux-todo-app/components/TodoItem.jsx
+++ react-redux-todo-app/components/TodoItem.jsx
@@ -1,10 +1,29 @@
-// eslint-disable-next-line react/prop-types, no-unused-vars
+import { useDispatch } from "react-redux";
+import { toggleComplete } from "../src/app/todoSlice";
+
+// eslint-disable-next-line react/prop-types
 const TodoItem = ({ id, title, completed }) => {
+  const dispatch = useDispatch();
+
+  const handleCompleteChange = (event) => {
+    event.preventDefault(); // Prevent default form submission behavior
+    dispatch(
+      toggleComplete({
+        id: id,
+        completed: !completed,
+      })
+    );
+  };
+
   return (
     <li className={`list-group-item ${completed && "list-group-item-success"}`}>
       <div className="d-flex justify-content-between">
         <span className="d-flex align-items-center">
-          <input type="checkbox" className="mr-3" checked={completed}></input>
+          <input
+            type="checkbox"
+            className="mr-3"
+            onChange={handleCompleteChange}
+          />
           {title}
         </span>
         <button className="btn btn-danger">Delete</button>
